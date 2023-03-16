1636b818616c93276c9041f84b4e9c3b9b910fd2
phonthakorn <phonthakorn@phonthakorns-MacBook-Pro-2.local>
2023-03-16 16:56:17 +0700

    hex format int

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ app = FastAPI()
 
 @app.post("/sign")
 async def sign(userkey: str = Form(), name: str = Form(), fs_source: UploadFile = File(), fs_pic_sign: UploadFile = File(None)):
-    keyID = bytes.fromhex(userkey)
+    keyID = bytes.fromhex(int(userkey))
     clshsm = HSM(name, keyID, "CryptoServer PKCS11 Token","12345")
     clshsm.existcert()
     
